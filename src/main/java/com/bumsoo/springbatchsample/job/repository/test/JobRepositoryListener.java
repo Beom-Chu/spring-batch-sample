a53b8ccdc9568841ff38be9c184ec9d16465f6cd
bumsoo <chorocsky@gmail.com>
2023-03-16 16:13:24 +0900

    JobRepository

---
+++ src/main/java/com/bumsoo/springbatchsample/job/repository/test/JobRepositoryListener.java
@@ -0,0 +1,38 @@
+package com.bumsoo.springbatchsample.job.repository.test;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.batch.core.*;
+import org.springframework.batch.core.repository.JobRepository;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+@Slf4j
+public class JobRepositoryListener implements JobExecutionListener {
+
+    private final JobRepository jobRepository;
+    @Override
+    public void beforeJob(JobExecution jobExecution) {
+
+    }
+
+    @Override
+    public void afterJob(JobExecution jobExecution) {
+        String jobName = jobExecution.getJobInstance().getJobName();
+        JobParameters jobParameters = new JobParametersBuilder()
+                .addString("name", "bumsoo").toJobParameters();
+
+        JobExecution lastJobExecution = jobRepository.getLastJobExecution(jobName, jobParameters);
+
+        if(lastJobExecution != null) {
+            for(StepExecution e : lastJobExecution.getStepExecutions()) {
+                log.info("[[[status : {}", e.getStatus());
+                log.info("[[[exitStatus : {}", e.getExitStatus());
+                log.info("[[[stepName : {}", e.getStepName());
+                log.info("[[[startTime : {}", e.getStartTime());
+                log.info("[[[endTime : {}", e.getEndTime());
+            }
+        }
+    }
+}
