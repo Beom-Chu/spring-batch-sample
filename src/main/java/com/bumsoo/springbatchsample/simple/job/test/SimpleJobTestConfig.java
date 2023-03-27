0feecd980619e5d17f46442b9013d9b3f1a61fbd
bumsoo <chorocsky@gmail.com>
2023-03-27 17:36:28 +0900

    SimpleJob incrementer test

--- src/main/java/com/bumsoo/springbatchsample/simple/job/test/SimpleJobTestConfig.java
+++ src/main/java/com/bumsoo/springbatchsample/simple/job/test/SimpleJobTestConfig.java
@@ -25,12 +25,13 @@ public class SimpleJobTestConfig {
                 .get("simpleJobTestJob")
                 .start(simpleJobTestStep1())
                 .next(simpleJobTestStep2())
-                .incrementer(new RunIdIncrementer())            /* JobParameter 의 값을 자동을 증가해 주는 JobParametersIncrementer 설정 */
-                .preventRestart()                               /* Job 의 재 시작 가능 여부 설정, 기본값 true */
-                .validator(new DefaultJobParametersValidator(   /* JobParameter 를 실행하기 전에 올바른 구성이 되었는지 검증하는 JobParametersValidator 설정 */
-                        new String[]{"name"}            /* requiredKeys : 필수 */
-                        ,new String[]{"age", "year"}    /* optionalKeys : 선택 */
-                ))
+//                .incrementer(new RunIdIncrementer())            /* JobParameter 의 값을 자동을 증가해 주는 JobParametersIncrementer 설정 */
+                .incrementer(new CustomJobParametersIncrementer())
+//                .preventRestart()                               /* Job 의 재 시작 가능 여부 설정, 기본값 true */
+//                .validator(new DefaultJobParametersValidator(   /* JobParameter 를 실행하기 전에 올바른 구성이 되었는지 검증하는 JobParametersValidator 설정 */
+//                        new String[]{"name"}            /* requiredKeys : 필수 */
+//                        ,new String[]{"age", "year"}    /* optionalKeys : 선택 */
+//                ))
 //                .validator(new CustomJobParametersValidator()) /* DefaultJobParametersValidator 사용해도 되고 Custom 생성도 가능 */
                 .listener(new JobExecutionListener() {          /* Job 라이프 사이클의 특정 시점에 콜백 제공받도록 JobExecutionListener 설정  */
                     @Override
