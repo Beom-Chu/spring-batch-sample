732d0c910fd60b90448bc4b67a4a922124c5185f
bumsoo <chorocsky@gmail.com>
2023-07-10 10:50:16 +0900

    ChunkOrientedTasklet

--- src/main/java/com/bumsoo/springbatchsample/chunk/composite/writer/test/TestCompositeWriterJobConfig.java
+++ src/main/java/com/bumsoo/springbatchsample/chunk/composite/writer/test/TestCompositeWriterJobConfig.java
@@ -1,4 +1,4 @@
-package com.bumsoo.springbatchsample.chunk.composite.writer;
+package com.bumsoo.springbatchsample.chunk.composite.writer.test;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.batch.core.Job;
